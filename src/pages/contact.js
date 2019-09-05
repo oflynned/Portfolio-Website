ea6c74e6a5b961cab5cce795cd14a0b926db2e54
Edmond O'Flynn <oflynned@tcd.ie>
2019-09-05 13:48:06 +0200

    add contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,7 +3,13 @@ import {Helmet} from "react-helmet";
 
 import headings from "../common/navHeadings";
 
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faEnvelopeOpen} from "@fortawesome/free-solid-svg-icons";
+import {faGithub, faLinkedin} from "@fortawesome/free-brands-svg-icons"
+
 import NavBar from "../components/nav/NavBar";
+
+import "./contact.css";
 
 class Contact extends Component {
     render() {
@@ -14,6 +20,41 @@ class Contact extends Component {
                 </Helmet>
 
                 <NavBar headings={headings} selectedIndex={4}/>
+
+                <div className={"content"}>
+                    <div className={"offset-page-content"}>
+                        <div className={"content-inner"}>
+                            <h1>Contact me</h1>
+                            <p>I am available for hire both on an employee and contractor basis. Feel free to reach out
+                                if you have any ideas for how we could work together.</p>
+
+                            <table>
+                                <tr>
+                                    <td>Email</td>
+                                    <td>
+                                        <FontAwesomeIcon icon={faEnvelopeOpen}/>
+                                        <a href={"mailto:oflynned@gmail.com"}>oflynned@gmail.com</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>LinkedIn</td>
+                                    <td>
+                                        <FontAwesomeIcon icon={faLinkedin}/>
+                                        <a href={"https://www.linkedin.com/in/edmond-o-flynn-41b06497"}
+                                           target={"_blank"}>edmond-o-flynn</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>GitHub</td>
+                                    <td>
+                                        <FontAwesomeIcon icon={faGithub}/>
+                                        <a href={"https://github.com/oflynned"} target={"_blank"}>oflynned</a>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
