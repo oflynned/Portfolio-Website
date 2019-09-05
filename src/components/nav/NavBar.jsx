0fe4c7e2f2bf68a75b89cad7212abe5adca05528
Edmond O'Flynn <oflynned@tcd.ie>
2019-09-05 12:32:36 +0200

    add a tag for opening static pdf

--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -3,16 +3,16 @@ import {Link} from "gatsby";
 
 import "./navBar.css"
 
-const NavBar = ({headings, selectedIndex, onClick}) =>
+const NavBar = ({headings, selectedIndex}) =>
     <div className={"nav-bar"}>
         <div className={"nav-content"}>
             <div className={"avatar"}>
-                <img src={require("../../assets/images/avatar.jpeg")}/>
+                <img src={require("../../assets/images/avatar.jpeg")} alt={"profile picture"}/>
             </div>
 
             <div className={"person-details"}>
                 <h1><strong>Edmond O'Flynn</strong></h1>
-                <h2>Full-Stack Web Developer</h2>
+                <h2>Full-Stack Developer</h2>
             </div>
 
             <div className={"nav-links"}>
@@ -20,8 +20,13 @@ const NavBar = ({headings, selectedIndex, onClick}) =>
                     {
                         headings.map(({title, link}, index) =>
                             <li className={selectedIndex === index ? "selected" : "unselected"}>
-                                <Link to={link}>{title}{selectedIndex === index ? " >" : ""}</Link>
-                            </li>)
+                                {
+                                    link.match(/pdf/) === null ?
+                                        <Link to={link}>{title}{selectedIndex === index ? " >" : ""}</Link> :
+                                        <a href={link} target={"_blank"}>{title}</a>
+                                }
+                            </li>
+                        )
                     }
                 </ul>
             </div>
