d8df5880c90e4df5054a98a36e4e5ee633626ae6
Edmond O'Flynn <oflynned@tcd.ie>
2019-09-05 11:16:55 +0200

    add fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,28 +4,28 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
-require("dotenv").config({ path: ".env" })
+require("dotenv").config({path: ".env"})
 
 const faviconConfig = {
-  resolve: "gatsby-plugin-favicon",
-  options: {
-    logo: "./src/assets/images/favicon.png",
-  },
+    resolve: "gatsby-plugin-favicon",
+    options: {
+        logo: "./src/assets/images/favicon.png",
+    },
 }
 
 const fontLoaderConfig = {
-  resolve: "gatsby-plugin-web-font-loader",
-  options: {
-    google: {
-      families: ["Quicksand", "Open Sans"],
+    resolve: "gatsby-plugin-web-font-loader",
+    options: {
+        google: {
+            families: ["Roboto", "Quicksand", "Open Sans"],
+        },
     },
-  },
 }
 
 module.exports = {
-  plugins: [
-    fontLoaderConfig,
-    faviconConfig,
-    "gatsby-plugin-react-helmet",
-  ],
+    plugins: [
+        fontLoaderConfig,
+        faviconConfig,
+        "gatsby-plugin-react-helmet",
+    ],
 }
