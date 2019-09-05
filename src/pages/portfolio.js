91c43a9d282263298964f55b84861b62faa7fcac
Edmond O'Flynn <oflynned@tcd.ie>
2019-09-05 19:14:55 +0200

    add portfolio page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -5,16 +5,37 @@ import headings from "../common/navHeadings";
 
 import NavBar from "../components/nav/NavBar";
 
+import "./portfolio.css";
+
+const tabs = ["All"];
+
 class Portfolio extends Component {
+    constructor() {
+        super();
+        this.state = {
+            selectedTabIndex: 0
+        }
+    }
+
     render() {
-        return <div className={"portfolio"}>
+        return (
+            <div className={"portfolio"}>
+                <Helmet>
+                    <title>Edmond O'Flynn | Software Engineer</title>/>
+                </Helmet>
 
-            <Helmet>
-                <title>Edmond O'Flynn | Software Engineer</title>/>
-            </Helmet>
+                <NavBar headings={headings} selectedIndex={1}/>
 
-            <NavBar headings={headings} selectedIndex={1}/>
-        </div>
+                <div className={"content"}>
+                    <div className={"offset-page-content"}>
+                        <div className={"content-inner"}>
+                            <h1>Portfolio</h1>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
     }
 }
 
