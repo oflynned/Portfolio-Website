34fd0dd2590234e2ea021879f54cb4cbaff5149f
Edmond O'Flynn <oflynned@tcd.ie>
2019-09-05 11:18:14 +0200

    add nav headings helper

---
+++ src/common/navHeadings.js
@@ -0,0 +1,24 @@
+const headings = [
+    {
+        title: "About Me",
+        link: "/"
+    },
+    {
+        title: "My Portfolio",
+        link: "/portfolio"
+    },
+    {
+        title: "My Offer",
+        link: "/offer"
+    },
+    {
+        title: "My CV",
+        link: "/cv"
+    },
+    {
+        title: "Contact Me",
+        link: "/contact"
+    }
+];
+
+export default headings;
