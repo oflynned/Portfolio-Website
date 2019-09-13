299264d48649b1a047607fb6751dfa224ea217c6
Edmond O'Flynn <oflynned@tcd.ie>
2019-09-13 17:00:53 +0100

    add project template

--- src/pages/project.js
+++ src/pages/project.js
@@ -1,26 +1,20 @@
-import React, {Component} from "react";
+import React from "react";
 
 import NavBar from "../components/nav/NavBar";
 
-import "./projects.css";
+import "./project.css";
 
-class Projects extends Component {
-    render() {
-        return (
-            <div className={"project"}>
-
-                <NavBar selectedIndex={1}/>
-
-                <div className={"content"}>
-                    <div className={"offset-page-content"}>
-                        <div className={"content-inner"}>
-                            <h1>Project Name</h1>
-                        </div>
-                    </div>
+const Project = ({pageContext: {project}}) => (
+    <div className={"project"}>
+        <NavBar selectedIndex={1}/>
+        <div className={"content"}>
+            <div className={"offset-page-content"}>
+                <div className={"content-inner"}>
+                    <h1>{project.title}</h1>
                 </div>
             </div>
-        );
-    }
-}
+        </div>
+    </div>
+);
 
-export default Projects;
+export default Project;
