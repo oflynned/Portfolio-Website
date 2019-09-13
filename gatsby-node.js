fa77a44e3df5e1c2bd532d2bd4ca848ea8a45a2f
Edmond O'Flynn <oflynned@tcd.ie>
2019-09-13 17:27:19 +0100

    fix multiple spacing issue

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,7 +6,7 @@ exports.createPages = ({actions}) => {
     const template = path.resolve(`./src/components/templates/project.js`);
     return new Promise((resolve, reject) => {
         projects.forEach(project => {
-            const title = project.title.toLowerCase().replace(" ", "-");
+            const title = project.title.toLowerCase().replace(/ /g, "-");
             createPage({
                 path: `/portfolio/${title}`,
                 component: template,
