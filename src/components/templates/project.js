fd1fcc1bb84589604980b3f66991bbf00c31fb31
Edmond O'Flynn <oflynned@tcd.ie>
2019-09-13 17:21:47 +0100

    add some basic sections for portfolio

--- src/components/templates/project.js
+++ src/components/templates/project.js
@@ -11,6 +11,13 @@ const Project = ({pageContext: {project}}) => (
             <div className={"offset-page-content"}>
                 <div className={"content-inner"}>
                     <h1>{project.title}</h1>
+                    <h3>{project.tags.join(", ")}</h3>
+                    <p>{project.description}</p>
+
+                    <h2>Project Requirements</h2>
+                    <h2>Project Overview</h2>
+                    <h2>Challenge</h2>
+                    <h2>Approach &amp; Solution</h2>
                 </div>
             </div>
         </div>
