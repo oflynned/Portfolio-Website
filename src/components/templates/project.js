3eaf2c8d3dedf3c259195fe0c85b7374cea7aea0
Edmond O'Flynn <oflynned@tcd.ie>
2019-09-14 18:12:56 +0100

    add more info to project view

--- src/components/templates/project.js
+++ src/components/templates/project.js
@@ -3,6 +3,7 @@ import React from "react";
 import NavBar from "../nav/NavBar";
 
 import "./project.css";
+import Laptop from "../layout/laptop";
 
 const Project = ({pageContext: {project}}) => (
     <div className={"project"}>
@@ -11,13 +12,18 @@ const Project = ({pageContext: {project}}) => (
             <div className={"offset-page-content"}>
                 <div className={"content-inner"}>
                     <h1>{project.title}</h1>
-                    <h3>{project.tags.join(", ")}</h3>
+                    <p>{project.tags.sort().join(", ")}</p>
                     <p>{project.description}</p>
+                    <p>Project requirements were as follows:</p>
+                    <ul>
+                        {project.requirements.map(r => <li>{r}</li>)}
+                    </ul>
 
-                    <h2>Project Requirements</h2>
-                    <h2>Project Overview</h2>
-                    <h2>Challenge</h2>
-                    <h2>Approach &amp; Solution</h2>
+                    <h2>What is {project.title}?</h2>
+                    {project.overview.map(o => <p>{o}</p>)}
+
+                    <h2>Showcase</h2>
+                    <Laptop images={project.showcase.desktop}/>
                 </div>
             </div>
         </div>
