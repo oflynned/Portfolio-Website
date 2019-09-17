7f02098c7f36db7df5186c4ad5e309d941884c11
Edmond O'Flynn <oflynned@tcd.ie>
2019-09-17 15:34:50 +0100

    add android impl of class frame

--- src/components/layout/android.jsx
+++ src/components/layout/android.jsx
@@ -3,10 +3,43 @@ import React, {Component} from "react";
 import "./android.css";
 
 class Android extends Component {
+    constructor() {
+        super();
+        this.state = {
+            index: null,
+            images: []
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            index: 0,
+            images: this.props.images || []
+        }, this.transitionImage());
+    }
+
+    transitionImage() {
+        setTimeout(() => {
+            const {index, images} = this.state;
+            if (index < images.length - 1) {
+                this.setState({index: index + 1})
+            } else {
+                this.setState({index: 0})
+            }
+
+            this.transitionImage();
+        }, 5000);
+    }
+
     render() {
+        const {index, images} = this.state;
         return (
-            <div className={"android"}/>
-        )
+            <div className={"android"}
+                 style={{backgroundImage: 'url("/pixel-outline.png")'}}>
+                <div className={"inner-content"}
+                     style={{backgroundImage: 'url("' + images[index] + '")'}}/>
+            </div>
+        );
     }
 }
 
